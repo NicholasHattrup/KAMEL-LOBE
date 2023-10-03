50981b80476815672ee28aea582c3bfc8bc3f138
NicholasHattrup <nhattrup@andrew.cmu.edu>
2023-10-03 14:32:48 -0400

    Optimized some operations for less memory and faster performance with sparse matrices

--- KAMEL_LOBE.py
+++ KAMEL_LOBE.py
@@ -29,32 +29,29 @@ def KAMEL_LOBE(r,RDF,w=0.015):
         M1 = np.identity(Nbins) + np.tril(2*np.ones((Nbins,Nbins)), -1)
         M1[:,0] = 1
         M1[0,:] = 0
-        M2 = np.diags(np.arange(0, Nbins)**2)*delr**2 # 
-        T1 = 2*np.pi*delr*M1@M2
+        M2 = (np.arange(0, Nbins)**2)*delr**2 # 
+        T1 = 2*np.pi*delr*M1*M2
 
         # Computing T2
         k_KL = 2*m_KL-1
         fractions = np.zeros((1,k_KL))
-        A1_block = np.identity(m_KL)
-        A2_block = np.zeros((m_KL, Nbins-m_KL))
-        C1_block = A2_block.copy()
-        C2_block = A1_block.copy()
+        A1_block = sp.identity(m_KL, format='csr')
+        A2_block = sp.lil_matrix((m_KL, Nbins-m_KL))
         fractions[0,m_KL-1:] = norm.cdf(((np.arange(0,m_KL)+0.5)*delr),0,w)-norm.cdf(((np.arange(0,m_KL)-0.5)*delr),0,w)        
         fractions[0,:m_KL-1] = np.flip(fractions[0,m_KL:2*m_KL-1])
         fractions[0, :] *= 1/np.sum(fractions)
-        B_block = sp.diags(np.tile(fractions, (Nbins-2*m_KL, 1)).T, np.arange(0, 2*(m_KL-1)+1), shape=(Nbins-2*m_KL, Nbins)).toarray()
-        T2 = np.vstack((np.hstack((A1_block, A2_block)), B_block, np.hstack((C1_block, C2_block))))
-
+        B_block = sp.diags(np.tile(fractions, (Nbins-2*m_KL, 1)).T, np.arange(0, 2*(m_KL-1)+1), shape=(Nbins-2*m_KL, Nbins))
+        T2 = sp.vstack((sp.hstack((A1_block, A2_block)), B_block, sp.hstack((A2_block, A1_block))))
+        
         # Computing T3
         sq_r = np.zeros((Nbins,1))
         sq_r[1:,0] = (np.arange(1,Nbins)*delr)**-2
-        r_exploded_out = np.tile(sq_r, (1, Nbins))
         K = (-1)**(np.arange(1,Nbins+1)+1)
         K_sq = np.outer(K,K)
-        KTr = 2*np.tril(K_sq)
+        KTr = 2*np.tril(K_sq).astype(float)
         np.fill_diagonal(KTr, 1) 
-        Kdiff = (2/delr)*KTr
-        T3 = (1/(4.*np.pi))*r_exploded_out*Kdiff
+        KTr *= (2/delr)
+        T3 = (1/(4.*np.pi))*sq_r*KTr
 
         # Computing gr_tilde
         gr_convert = T3@T2@T1@RDF
